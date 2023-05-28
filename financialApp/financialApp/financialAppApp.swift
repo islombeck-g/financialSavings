09aa8eecf173a6e10a7483032f2c4f04573dbf14
islombeck-g <whoisit864@gmail.com>
2023-05-28 12:17:54 +0300

    firstCodeCommit

---
+++ financialApp/financialApp/financialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  financialAppApp.swift
+//  financialApp
+//
+//  Created by Islombek Gofurov on 28.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct financialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
